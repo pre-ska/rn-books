d7569afbd172b6df01796e91faab89e07d29224c
preska <wolfwar@outlook.com>
2020-01-15 12:22:52 +0100

    Delete config.js

--- config/config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyD3Yvgyuju9b_LJgYjG6H5WkiOKxVdW0r4",
-  authDomain: "rn-bookmarx.firebaseapp.com",
-  databaseURL: "https://rn-bookmarx.firebaseio.com",
-  projectId: "rn-bookmarx",
-  storageBucket: "rn-bookmarx.appspot.com",
-  messagingSenderId: "59880780741",
-  appId: "1:59880780741:web:6be3dcd36465048bde51ac"
-};
